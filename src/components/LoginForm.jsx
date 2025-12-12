b6a4bf4a13729c2acd1bac8313db471cc7b4f491
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-12-11 21:18:52 -0300

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import IForumBg from '../assets/iforum_logo.png'; 
+import IForumLogo from '../assets/iforum_logo.png';
 import './LoginForm.css'; 
 
 const LoginForm = () => {
@@ -19,39 +20,50 @@ const LoginForm = () => {
         <div className="login-form-wrapper" style={loginStyle}>
             
             <div className="login-form-container">
-                <h2 className="login-title">Login de Acesso</h2>
+                
+                {}
+                <div className="icon-avatar-container">
+                    <i className="icon-user-avatar">👤</i> {}
+                </div>
+                
+                <h2 className="login-title">Acesso ao Sistema</h2>
                 
                 <form onSubmit={handleSubmit}>
                     
                     {}
                     <div className="form-group">
                         <label htmlFor="email" className="form-label">E-mail</label>
-                        <input
-                            type="email"
-                            id="email"
-                            placeholder="Digite seu e-mail"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            className="input-field" 
-                        />
+                        <div className="input-icon-group">
+                            <i className="icon-email">📧</i> {}
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Digite seu e-mail"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                                className="input-field with-icon" 
+                            />
+                        </div>
                     </div>
                     
                     {}
                     <div className="form-group">
                         <label htmlFor="password" className="form-label">Senha</label>
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Digite sua senha"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            className="input-field" 
-                        />
+                        <div className="input-icon-group">
+                            <i className="icon-lock">🔒</i> {}
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Digite sua senha"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                className="input-field with-icon" 
+                            />
+                        </div>
                     </div>
                     
-                    {}
                     <button 
                         type="submit" 
                         className="primary-button" 
